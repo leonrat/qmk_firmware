d7008795be9c8a8812d1965e86b2456ede828274
Léon R <leon.ratinckx@skynet.be>
2024-02-12 18:40:41 +0100

    safety push

--- keyboards/chocobanan/config.h
+++ keyboards/chocobanan/config.h
@@ -2,15 +2,18 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-#define SPLIT_HAND_PIN B5
+#define SPLIT_HAND_PIN GP9
 #define MASTER_LEFT
-#define SOFT_SERIAL_PIN D3
-#define ENCODERS_PAD_A { F4 }
-#define ENCODERS_PAD_B { F5 }
+#define SOFT_SERIAL_PIN GP0
+#define ENCODERS_PAD_A { GP29 }
+#define ENCODERS_PAD_B { GP28 }
 #define ENCODER_RESOLUTION 2
 #define MOUSE_EXTENDED_REPORT
 #define RGBLED_NUM 6
-#define WS2812_DI_PIN D2
+#define WS2812_DI_PIN GP1
+
+
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
